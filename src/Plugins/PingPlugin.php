910d5bc3f24b6bc8f83328947a07b2ce6ebc6348
Pascal Pirschel <pascal.pirschel@gmail.com>
2021-01-02 03:30:18 +0100

    Added a longer ping message for testing & example reasons

--- src/Plugins/PingPlugin.php
+++ src/Plugins/PingPlugin.php
@@ -10,8 +10,14 @@ class PingPlugin extends BasePlugin
         'ping' => 'ping'
     ];
 
-    public function ping(string $message, string $channelId)
+    public function ping(string $message, string $channelId, array $messageObject)
     {
-        $this->sendMessage('Pong', $channelId);
+        $userId = !empty($messageObject['webhook_id']) ? null : $messageObject['author']['id'];
+        if ($userId && $message == 'long') {
+            $response = "Hey <@${userId}>. I'm up and running, and having fun!";
+            $this->sendMessage($response, $channelId);
+        } else {
+            $this->sendMessage('Pong', $channelId);
+        }
     }
 }

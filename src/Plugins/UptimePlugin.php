8b1fc56e436a8243a701cc5be25c799dc0fc4176
Pascal Pirschel <pirschel@lamapoll.de>
2021-01-14 12:23:54 +0100

    Fix uptime padding

--- src/Plugins/UptimePlugin.php
+++ src/Plugins/UptimePlugin.php
@@ -25,7 +25,7 @@ class UptimePlugin extends BasePlugin
     {
         $startTime = $this->initTime->format('Y-m-d H:i T');
         $uptime = (new DateTime())->diff($this->initTime);
-        $strUptime = str_pad($uptime->days ?: 0, 3, '0') . $uptime->format(':%H:%I:%S');
+        $strUptime = str_pad($uptime->days ?: 0, 3, '0', STR_PAD_LEFT) . $uptime->format(':%H:%I:%S');
 
         $this->sendMessage("I've been up and running since ${startTime} (for ${strUptime})", $message->getChannelId());
     }

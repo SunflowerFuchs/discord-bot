580391e18bf5246522aeaed1f7a36fcde2bda7e1
Pascal Pirschel <pirschel@lamapoll.de>
2022-04-11 15:26:53 +0200

    Cleaned up role object

--- src/Api/Objects/Role.php
+++ src/Api/Objects/Role.php
@@ -4,19 +4,58 @@ declare(strict_types=1);
 
 namespace SunflowerFuchs\DiscordBot\Api\Objects;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use SunflowerFuchs\DiscordBot\Api\Constants\Headers;
 use SunflowerFuchs\DiscordBot\Api\Constants\Permissions;
+use SunflowerFuchs\DiscordBot\Bot;
 
 class Role
 {
+    /**
+     * role id
+     */
     protected Snowflake $id;
+    /**
+     * role name
+     */
     protected string $name;
+    /**
+     * integer representation of hexadecimal color code
+     */
     protected int $color;
+    /**
+     * if this role is pinned in the user listing
+     */
     protected bool $hoist;
+    /**
+     * role icon hash
+     */
+    protected ?string $icon;
+    /**
+     * role unicode emoji
+     */
+    protected ?string $unicode_emoji;
+    /**
+     * position of this role
+     */
     protected int $position;
+    /**
+     * permission bit set
+     */
     protected int $permissions;
+    /**
+     * whether this role is managed by an integration
+     */
     protected bool $managed;
+    /**
+     * whether this role is mentionable
+     */
     protected bool $mentionable;
-    protected RoleTags $tags;
+    /**
+     * the tags this role has
+     */
+    protected ?RoleTags $tags;
 
     public function __construct(array $data)
     {
@@ -24,11 +63,195 @@ class Role
         $this->name = $data['name'];
         $this->color = $data['color'];
         $this->hoist = $data['hoist'];
+        $this->icon = $data['icon'] ?? null;
+        $this->unicode_emoji = $data['unicode_emoji'] ?? null;
         $this->position = $data['position'];
         $this->permissions = (int)$data['permissions'];
         $this->managed = $data['managed'];
         $this->mentionable = $data['mentionable'];
         $this->tags = new RoleTags($data['tags'] ?? []);
+    }
+
+    /**
+     * @param Client $apiClient
+     * @param Snowflake $guildId
+     * @param string $name
+     * @param int $permissions
+     * @param int $color
+     * @param bool $hoist
+     * @param string $icon
+     * @param string $unicodeEmoji
+     * @param bool $mentionable
+     * @param string $reason
+     * @return ?static
+     * @throws GuzzleException
+     */
+    public static function create(
+        Client $apiClient,
+        Snowflake $guildId,
+        string $name = 'new role',
+        int $permissions = 0,
+        int $color = 0,
+        bool $hoist = false,
+        string $icon = '',
+        string $unicodeEmoji = '',
+        bool $mentionable = false,
+        string $reason = ''
+    ): ?self {
+        $params = [
+            'name' => $name,
+            'permissions' => $permissions,
+            'color' => $color,
+            'hoist' => $hoist,
+            'icon' => $icon,
+            'unicodeEmoji' => $unicodeEmoji,
+            'mentionable' => $mentionable,
+        ];
+
+        $options = [
+            'json' => $params
+        ];
+        if (!empty($reason)) {
+            $options['headers'] = [Headers::AUDIT_LOG_REASON => $reason];
+        }
+
+        $res = $apiClient->post("guilds/${guildId}/roles", $options);
+        if ($res->getStatusCode() === 200) {
+            return new static(json_decode($res->getBody()->getContents(), true));
+        }
+
+        return null;
+    }
+
+    /**
+     * @param Client $apiClient
+     * @param Snowflake $guildId
+     * @param Snowflake $roleId
+     * @param ?string $name
+     * @param ?int $permissions
+     * @param ?int $color
+     * @param ?bool $hoist
+     * @param ?string $icon
+     * @param ?string $unicodeEmoji
+     * @param ?bool $mentionable
+     * @param string $reason
+     * @return ?static
+     * @throws GuzzleException
+     */
+    public static function modify(
+        Client $apiClient,
+        Snowflake $guildId,
+        Snowflake $roleId,
+        ?string $name = null,
+        ?int $permissions = null,
+        ?int $color = null,
+        ?bool $hoist = null,
+        ?string $icon = null,
+        ?string $unicodeEmoji = null,
+        ?bool $mentionable = null,
+        string $reason = ''
+    ): ?self {
+        $params = [
+            'name' => $name,
+            'permissions' => $permissions,
+            'color' => $color,
+            'hoist' => $hoist,
+            'icon' => $icon,
+            'unicodeEmoji' => $unicodeEmoji,
+            'mentionable' => $mentionable,
+        ];
+
+        $options = [
+            'json' => $params
+        ];
+        if (!empty($reason)) {
+            $options['headers'] = [Headers::AUDIT_LOG_REASON => $reason];
+        }
+
+        $res = $apiClient->patch("guilds/${guildId}/roles/${roleId}", $options);
+        if ($res->getStatusCode() === 200) {
+            return new static(json_decode($res->getBody()->getContents(), true));
+        }
+
+        return null;
+    }
+
+    /**
+     * @param Client $apiClient
+     * @param Snowflake $guildId
+     * @param Snowflake $roleId
+     * @param int $position
+     * @param string $reason
+     * @return static[]
+     * @throws GuzzleException
+     */
+    public static function move(
+        Client $apiClient,
+        Snowflake $guildId,
+        Snowflake $roleId,
+        int $position,
+        string $reason = ''
+    ): array {
+        $params = [
+            'id' => "$roleId",
+            'position' => $position,
+        ];
+
+        $options = [
+            'json' => $params
+        ];
+        if (!empty($reason)) {
+            $options['headers'] = [Headers::AUDIT_LOG_REASON => $reason];
+        }
+
+        $res = $apiClient->patch("guilds/${guildId}/roles", $options);
+        if ($res->getStatusCode() === 200) {
+            return array_map(fn(array $roleData) => new static($roleData),
+                json_decode($res->getBody()->getContents(), true));
+        }
+
+        return [];
+    }
+
+    /**
+     * @param Client $apiClient
+     * @param Snowflake $guildId
+     * @param Snowflake $roleId
+     * @param string $reason
+     * @return bool
+     * @throws GuzzleException
+     */
+    public static function delete(
+        Client $apiClient,
+        Snowflake $guildId,
+        Snowflake $roleId,
+        string $reason = ''
+    ): bool {
+        $options = [];
+        if (!empty($reason)) {
+            $options['headers'] = [Headers::AUDIT_LOG_REASON => $reason];
+        }
+
+        $res = $apiClient->delete("guilds/${guildId}/roles/${roleId}", $options);
+        return $res->getStatusCode() === 204;
+    }
+
+    /**
+     * @param Client $apiClient
+     * @param Snowflake $guildId
+     * @return static[]
+     * @throw
+     * @throws GuzzleException
+     */
+    public static function listForGuild(Client $apiClient, Snowflake $guildId): array
+    {
+        $res = $apiClient->get("guilds/${guildId}/roles");
+        if ($res->getStatusCode() === 200) {
+            return array_map(fn(array $roleData) => new static($roleData),
+                json_decode($res->getBody()->getContents(), true));
+        }
+
+        return [];
     }
 
     /**
@@ -70,11 +293,11 @@ class Role
     }
 
     /**
-     * Whether the role is pinned in the user listing
+     * whether the role should be displayed separately in the sidebar
      *
      * @return bool
      */
-    public function isHoist(): bool
+    public function isHoisted(): bool
     {
         return $this->hoist;
     }
@@ -132,10 +355,20 @@ class Role
     /**
      * The tags this role has
      *
-     * @return RoleTags
+     * @return ?RoleTags
      */
-    public function getTags(): RoleTags
+    public function getTags(): ?RoleTags
     {
         return $this->tags;
+    }
+
+    /**
+     * Whether this role belongs to the given bot
+     * @param Bot $bot
+     * @return bool
+     */
+    public function belongsToBot(Bot $bot): bool
+    {
+        return $this->isManaged() && $this->getTags() !== null && $this->getTags()->getBotId() === $bot->getUserId();
     }
 }
